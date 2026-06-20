62a81e08e1a1ace5ed4e0b89543b33b4b2b899d1
Megan He <mhe@g.harvard.edu>
2026-06-20 13:05:35 -0400

    modify unit conversions

--- planeflight_functions.py
+++ planeflight_functions.py
@@ -5,104 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.constants import Boltzmann as kB
 
-def load_planeflight_ATom(rundir, campaign, region=None):
-    '''
-    Reads in GEOS-Chem sampled output planeflight.log files (ATom BY CAMPAIGN). 
-    For ATom, groups chemical species by latitude (0-30 N/S) and pressure bins.
-
-    Args:
-        rundir: directory containing ATom planeflight.log files
-        campaign: 1, 2, 3, or 4
-        region: 'Pacific' or 'Atlantic', for ATom
-
-    Returns:
-        grouped DataFrame with mean values (OHR: s-1, OH: molec/cm3, CO: ppb, temp: K) in pressure bins.
-    '''
-
-    files = glob.glob(f'{rundir}/ATom_logs/ATom{campaign}/plane.ATom.log.*')
-    files.sort()
-
-    # get header from first file
-    with open(files[0], 'r') as f:
-        header_line = f.readline().strip().split()
-
-    cols = ['LAT', 'LON', 'PRESS', 'CO', 'OH', 'OHR_99999', 'GMAO_TEMP']
-    usecols = [header_line.index(col) for col in cols]
-
-    # Load all files
-    all_dfs = []
-    for f in files:
-        df = pd.read_csv(
-            f,
-            delim_whitespace=True,
-            header=0,
-            usecols=usecols,
-            names=header_line
-        )
-        all_dfs.append(df)
-
-    # Combine all into one DataFrame
-    combined_df = pd.concat(all_dfs, ignore_index=True)
-
-    # group by hemisphere and group pressure bins
-    # df_NH = combined_df[combined_df['LAT'] > 0].copy()
-    # df_SH = combined_df[combined_df['LAT'] < 0].copy()
-
-    # group by ocean
-    if region == 'Pacific': # longitude from 120 to -80
-        combined_df = combined_df[(combined_df['LON'] > 120) | (combined_df['LON'] < -80)].copy()
-    elif region == 'Atlantic': # longitude from -75 to 10
-        combined_df = combined_df[combined_df['LON'].between(-75, 10)].copy()
-
-    # group by 0-30N and 0-30S, 30-60N
-    df_0to30N = combined_df[(combined_df['LAT'] > 0) & (combined_df['LAT'] < 30)].copy()
-    df_0to30S = combined_df[(combined_df['LAT'] < 0) & (combined_df['LAT'] > -30)].copy()
-    df_30to60N = combined_df[(combined_df['LAT'] > 30) & (combined_df['LAT'] < 60)].copy()
-
-    df_0to30N['PRESS_bin'] = (df_0to30N['PRESS'] // 50) * 50
-    df_0to30S['PRESS_bin'] = (df_0to30S['PRESS'] // 50) * 50
-    df_30to60N['PRESS_bin'] = (df_30to60N['PRESS'] // 50) * 50
-
-    # Get mean in each pressure bin
-    df_0to30N_mean = (
-        df_0to30N
-        .groupby('PRESS_bin')
-        .agg(
-            OH_mean      = ('OH',        'mean'),
-            OHR_mean     = ('OHR_99999', 'mean'),
-            CO_mean      = ('CO',        'mean'),
-            temp_mean    = ('GMAO_TEMP', 'mean'),
-        )
-        .reset_index()
-    )
-
-    df_0to30S_mean = (
-        df_0to30S
-        .groupby('PRESS_bin')
-        .agg(
-            OH_mean      = ('OH',        'mean'),
-            OHR_mean     = ('OHR_99999', 'mean'),
-            CO_mean      = ('CO',        'mean'),
-            temp_mean    = ('GMAO_TEMP', 'mean'),
-        )
-        .reset_index()
-    )
-
-    df_30to60N_mean = (
-        df_30to60N
-        .groupby('PRESS_bin')
-        .agg(
-            OH_mean      = ('OH',        'mean'),
-            OHR_mean     = ('OHR_99999', 'mean'),
-            CO_mean      = ('CO',        'mean'),
-            temp_mean    = ('GMAO_TEMP', 'mean'),
-        )
-        .reset_index()
-    )
-
-    return df_0to30N_mean, df_0to30S_mean, df_30to60N_mean
-
-
 def load_planeflight_ATom_all(rundir):
     '''
     Reads in GEOS-Chem sampled output planeflight.log files (ALL ATom campaigns). 
@@ -184,12 +86,12 @@ def load_planeflight_KORUSAQ(rundir):
     combined_df = pd.concat(all_dfs, ignore_index=True)
 
     # group by pressure bin only
-    combined_df['PRESS_bin'] = (combined_df['PRESS'] // 50) * 50
+    combined_df['PRESS'] = (combined_df['PRESS'] // 50) * 50
 
     # Get mean in each pressure bin
     mean_df = (
         combined_df
-        .groupby('PRESS_bin')
+        .groupby('PRESS')
         .agg(
             OH_mean      = ('OH',        'mean'),
             OHR_mean     = ('OHR_99999', 'mean'),
@@ -323,20 +225,21 @@ def convert_pressure_to_altitude(df):
         df: DataFrame with 'altitude_km' column added, in km
     
     '''
-    p_pa = df['PRESS_bin'] * 100
+    p_pa = df['PRESS'] * 100
     z_m = pressure_to_altitude(p_pa.values)
     df['altitude_km'] = z_m / 1000
 
     return df
 
 ### Unit conversions
-def molec_cm3_to_vv(conc_molec_cm3, alt_km, output):
+def molec_cm3_to_vv(conc_molec_cm3, P_Pa, T_K, output):
     '''
     Convert molec/cm3 to ppbv or pptv at a given altitude
 
     Args:
         conc_molec_cm3: concentration in molec/cm3
-        alt_km: altitude in km
+        P_pa: observed P
+        T_K: observed T
         output: 'ppbv' or 'pptv'
     Returns:
         concentration in ppbv or pptv
@@ -344,9 +247,6 @@ def molec_cm3_to_vv(conc_molec_cm3, alt_km, output):
     '''
     R_star = 8.3144598 # universal gas constant J/K/mol
     avogadro = 6.023e23 
-
-    P_Pa, T_K, _ = altitude_to_many(np.asarray(alt_km) * 1000)
-    
     # compute number density of air (molec/cm3)
     n_air_cm3 = (avogadro * P_Pa) / (R_star * T_K) * 1e-6
 
@@ -358,13 +258,14 @@ def molec_cm3_to_vv(conc_molec_cm3, alt_km, output):
     return (np.asarray(conc_molec_cm3) / n_air_cm3) * conversion
 
 
-def vv_to_molec_cm3(vv, alt_km, input_units):
+def vv_to_molec_cm3(vv, P_Pa, T_K, input_units):
     '''
     Convert ppbv or pptv to molec/cm3 at a given altitude
 
     Args:
         vv: mixing ratio in ppbv or pptv
-        alt_km: altitude in km
+        P_pa: observed P
+        T_K: observed T
         unit: 'ppbv' or 'pptv'
 
     Returns:
@@ -372,10 +273,6 @@ def vv_to_molec_cm3(vv, alt_km, input_units):
     '''
     R_star    = 8.3144598  # universal gas constant J/K/mol
     avogadro  = 6.023e23  
-
-    P_Pa, T_K, _ = altitude_to_many(np.asarray(alt_km) * 1e3)
-
-    # compute number density of air (molec/cm3)
     n_air_cm3 = (avogadro * P_Pa) / (R_star * T_K) * 1e-6
 
     if input_units == 'ppbv':
@@ -383,8 +280,7 @@ def vv_to_molec_cm3(vv, alt_km, input_units):
     elif input_units == 'pptv':
         conversion = 1e12
 
-    # conc = vv_fraction * n_air
-    return (np.asarray(vv) / conversion) * n_air_cm3
+    return (vv / conversion) * n_air_cm3
 
 # function to calculate k_CH4+OH given temperature in K
 def calc_k(T):
